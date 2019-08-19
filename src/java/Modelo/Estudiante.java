04c79e8893920160c984536547a44b19fadbc550
Verito <Verito@vvillarreal-ltp>
2019-08-19 11:59:21 -0500

    Init commit

--- src/java/Modelo/Estudiante.java
+++ src/java/Modelo/Estudiante.java
@@ -6,5 +6,42 @@ public class Estudiante {
   String ced;
   String nom;
   String ape;
+
+    public Estudiante(String ced) {
+       
+    }
+
+    public Estudiante(String nom, String ape) {
+        this.nom = nom;
+        this.ape = ape;
+    }
+
+    public String getCed() {
+        return ced;
+    }
+
+    public void setCed(String ced) {
+        this.ced = ced;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getApe() {
+        return ape;
+    }
+
+    public void setApe(String ape) {
+        this.ape = ape;
+    }
+
+   
+  
+   
   
 }
